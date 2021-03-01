0843544d6f22c2c5b49c437780f566811d3c34ad
Anton Berdnikov <agratoth@yandex.ru>
2021-03-01 19:23:43 +0400

    [0.1.5] Add pipepool to ODMApp constructor

--- pyasyncserver/apps/odm_app.py
+++ pyasyncserver/apps/odm_app.py
@@ -2,5 +2,7 @@ from .app import App
 
 
 class ODMApp(App):
-    def __init__(self, **kwargs):
+    def __init__(self, pipepool, **kwargs):
         self._storage = kwargs.get('storage', None)
+
+        App.__init__(self, pipepool)
